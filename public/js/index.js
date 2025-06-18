3dd79c6afb9d9e0927251ea4a4e21297a00bcad8
JustJill813 <justjill813@gmail.com>
2025-06-18 12:49:45 -0400

    updated to hide API Key - not working

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-async function getRandomImage() {
+/* async function getRandomImage() {
     const client_id = "kPegUbPEAJqDqU2384yEUcT3OSIGa89mExA7p49wztg";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
@@ -18,7 +18,7 @@ async function getRandomImage() {
     } catch (error) {
         console.error(error);
     }
-}
+} */
 
 getRandomImage();
 
